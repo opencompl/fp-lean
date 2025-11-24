99e470e48741909c1360976b8b84277f32052c8e
Siddharth Bhat <siddu.druid@gmail.com>
2025-11-24 17:08:17 +0000

    chore: basic infra to compare dyadics and fixed point

--- Fp/Proofs/Addition.lean
+++ Fp/Proofs/Addition.lean
@@ -1,4 +1,37 @@
+import Init.Data.Dyadic
 import Fp.Addition
 
--- We show that addition in the fixed-width case computes the same as the 
--- dyadic rationals.
+
+open Lean
+
+class HExOffset (e : Nat) (m : Nat) where
+  h : e < m
+
+
+instance HExOffsetSucc [hex : HExOffset e m] : HExOffset e (m + 1) where
+  h := by 
+    have := hex.h
+    omega
+    
+
+def FixedPoint.ofInt (i : Int) [HExOffset e m] : FixedPoint m e :=
+  {
+    sign := i ≥ 0
+    val := BitVec.ofNat m (i.natAbs)
+    hExOffset := HExOffset.h 
+  }
+
+def FixedPoint.ofDyadic (d : Dyadic) [HExOffset e m]  : FixedPoint m e :=
+  FixedPoint.ofInt (d.toRat.ceil) 
+
+
+/-- Show that a dyadic number corresponds to a fixed-point number. -/
+structure DyadicRefinesFixedPoint [HExOffset e m] (d : Dyadic) (f : FixedPoint m e) where
+   h : FixedPoint.ofDyadic d = f
+
+notation (name := dyadicSim) f "⊆d " d => (DyadicRefinesFixedPoint d f)
+
+theorem fp_add_dyadic [HExOffset e m] (da db : Dyadic) (fa fb : FixedPoint m e)
+ (ha : fa ⊆d da) (hb : fb ⊆d db) (mode : RoundingMode)
+  : (f_add mode fa fb) ⊆d  (Dyadic.add da db) := by
+  sorry
