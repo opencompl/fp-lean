198853888d8a1f741b11d1a42c8f8521c6bfa41c
Abdalrhman Mohamed <abdoo8080@outlook.com>
2025-12-09 21:57:24 +0000

    chore: replace bv_float_normalize decorations with bv_normalize (#14)
    chore: replace remaining bv_float_normalize decorations with bv_normalize

--- Fp/Rounding.lean
+++ Fp/Rounding.lean
@@ -63,11 +63,11 @@ Implemented in terms of bitblastable `clz`.
 Given a BV `000111`, then `clz` returns `3` (the number of leading zeros),
 and `fls` returns `6 - 3 = 3`, which is the index of the last set bit (counting from 1).
 -/
-@[simp, bv_float_normalize]
+@[simp, bv_normalize]
 def fls (b : BitVec n) : BitVec n := BitVec.ofNat n n - b.clz
 
 
-@[simp, bv_float_normalize]
+@[simp, bv_normalize]
 def fls_log (m : Nat) (b : BitVec n) : BitVec n :=
   if m = 0 then
     0
