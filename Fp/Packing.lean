f10608a73564889d8ea0a3d9195edf5380efb29b
Abdalrhman Mohamed <abdoo8080@outlook.com>
2025-12-16 21:37:29 +0000

    feat: implement pack and unpack operations (#24)
    * doc: document UnpackedFloat and EUnpackedFloat
    
    * feat: implement pack and unpack operations
    
    * chore: fix compilation bugs

---
+++ Fp/Packing.lean
@@ -0,0 +1,56 @@
+import Fp.Basic
+
+@[bv_normalize]
+def PackedFloat.unpack (pf : PackedFloat e s)
+  : EUnpackedFloat (exponentWidth e s) (s + 1) :=
+  bif pf.isNaN then
+    EUnpackedFloat.mkNaN
+  else bif pf.isInfinite then
+    EUnpackedFloat.mkInfinity pf.sign
+  else bif pf.isZero then
+    EUnpackedFloat.mkZero pf.sign
+  else bif pf.isNorm then
+    ({
+      sign := pf.sign
+      ex := pf.ex.zeroExtend _ - BitVec.ofNat _ (bias e)
+      sig := pf.sig.cons true
+      : UnpackedFloat _ _
+    }).toEUnpackedFloat
+  else -- bif pf.isSubnorm then
+    ({
+      sign := pf.sign
+      ex := BitVec.ofInt _ (minNormalExp e)
+      sig := pf.sig.cons false
+      : UnpackedFloat _ _
+    }).normalize.toEUnpackedFloat
+
+@[bv_normalize]
+def EUnpackedFloat.pack (uf : EUnpackedFloat (exponentWidth e s) (s + 1))
+  : PackedFloat e s :=
+  let inNormalRange := (BitVec.ofInt _ (minNormalExp e)).sle uf.exp
+  {
+    sign := uf.sign
+    ex := bif uf.isNaN || uf.isInfinite then
+            BitVec.allOnes e
+          else if uf.isZero || !inNormalRange then
+            BitVec.zero _
+          else -- bif uf.isNorm then
+            -- Truncate msbs used to normalize subnormals
+            (uf.exp + BitVec.ofNat _ (bias e)).truncate _
+    sig := bif uf.isNaN then
+            BitVec.ofNat _ (2 ^ (s - 1))
+           else bif uf.isInfinite || uf.isZero then
+            BitVec.zero _
+           else bif inNormalRange then
+            uf.sig.truncate _
+           else -- bif uf.isSubnorm then
+            let shift := BitVec.ofInt _ (minNormalExp e) - uf.exp
+            (uf.sig >>> shift).truncate _
+  }
+
+/-- info: { sign := +, ex := 0x00#5, sig := 0x001#10 } -/
+#guard_msgs in #eval { sign := false, ex := 0x00#5, sig := 0x001#10 : PackedFloat _ _ }.unpack.pack
+/-- info: { sign := +, ex := 0x0f#5, sig := 0x3ff#10 } -/
+#guard_msgs in #eval { sign := false, ex := 0x0f#5, sig := 0x3ff#10 : PackedFloat _ _ }.unpack.pack
+/-- info: { sign := +, ex := 0x00#5, sig := 0x000#10 } -/
+#guard_msgs in #eval { sign := false, ex := 0x00#5, sig := 0x000#10 : PackedFloat _ _ }.unpack.pack
--- Fp/Rounding.lean
+++ Fp/Rounding.lean
@@ -1,4 +1,5 @@
 import Fp.Basic
+import Fp.Packing
 
 /--
 Rounding modes used in floating-point calculations.
@@ -300,3 +301,23 @@ theorem ofRat_two_is_twoE5M2 : PackedFloat.ofRat 5 2 .RNE 2 1 = twoE5M2 := by
 #guard_msgs in #eval truncateRight 4 0xaf#8
 /-- info: 44800#16 -/
 #guard_msgs in #eval truncateRight 16 0xaf#8
+
+/-- info: 6#5 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).ex - BitVec.ofNat 5 (bias 5)
+/-- info: 1408#11 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).sig.cons true
+/-- info: 6#6 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).unpack.num.ex
+/-- info: 1408#11 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).unpack.num.sig
+/-- info: 88 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).toEFixed.num.toDyadic.toRat
+/-- info: 88 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).unpack.num.toDyadic.toRat
+/-- info: 88 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).unpack.num.toRat
+/-- info: 88 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE 88 1).unpack.pack.toEFixed.num.toDyadic.toRat
+
+/-- info: -88 -/
+#guard_msgs in #eval (PackedFloat.ofRat 5 (11 - 1) .RNE (-88) 1).unpack.pack.toEFixed.num.toDyadic.toRat
