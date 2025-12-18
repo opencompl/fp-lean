ac2ba0f20b615c9509c45ef8811e2eab95a264aa
Abdalrhman Mohamed <abdoo8080@outlook.com>
2025-12-18 08:22:44 -0800

    feat: finish the correctness proof for normalize
    Co-authored-by: Siddharth <siddu.druid@gmail.com>

--- Fp/Theorems/Packing.lean
+++ Fp/Theorems/Packing.lean
@@ -29,7 +29,6 @@ theorem BitVec.toNat_shiftLeft_clz (x : BitVec w)
   · grind
   · exact Nat.two_pow_pos x.clz.toNat
 
-
 namespace UnpackedFloat
 
 theorem toRat_eq {uf : UnpackedFloat e s}
@@ -71,35 +70,65 @@ theorem toRat_sig_zero_eq_toRat_mkZero {uf : UnpackedFloat e s}
   : uf.sig = 0 → uf.toRat = 0 := by
   simp +contextual [toRat]
 
+theorem Nat.log2_eq_exists (n : Nat) (hn : n ≠ 0) :
+  ∃ k, n.log2 = k ∧ 2 ^ k ≤ n ∧ n < 2 ^ (k + 1) := by
+  let k := n.log2
+  exists k
+  simp [k]
+  apply Nat.log2_eq_iff .. |>.mp <;> grind
+
 theorem two_mul_sigWidth_lt_exponentWidth :
-  2 * s < 2 ^ (exponentWidth e s) := by
-  simp only [exponentWidth]
-  sorry
+  2 * s ≤ 2 ^ (exponentWidth e s) := by
+  unfold exponentWidth
+  cases s with
+  | zero  => simp
+  | succ s =>
+    cases s with
+    | zero =>
+      simp [Nat.pow_add]
+      grind [Nat.two_pow_pos]
+    | succ s =>
+      have : 2 ^ (e - 1) + (s + 1 + 1) - 2 ≠ 0 := by grind [Nat.two_pow_pos]
+      have ⟨k, h, hl, hu⟩ := Nat.log2_eq_exists _ this
+      rw [h]
+      simp [Nat.pow_add]
+      suffices s + 2 ≤ 2 ^ k * 2 from by
+        grind
+      have : 2 ^ (e - 1) ≥ 1 := by grind [Nat.pow_pos]
+      have : 1 + s < 2 ^ k * 2 := by grind [Nat.pow_pos, Nat.pow_add]
+      grind
+      -- grind
 
 theorem toRat_normalize_eq_toRat {uf : UnpackedFloat e s}
-  (hse : 2 * s < 2 ^ e)
+  (hse : 2 * s ≤ 2 ^ e)
   (hex : !uf.ex.ssubOverflow (BitVec.setWidth e uf.sig.clz))
   : uf.normalize.toRat = uf.toRat := by
-  -- simp only []
   simp only [normalize, Bool.cond_eq_ite, beq_iff_eq, BitVec.clz_eq_iff_eq_zero]
   split
-  · simp_all []
-  · simp only [toRat_eq, Rat.mul_assoc]
+  · simp_all
+  case isFalse h =>
+    simp only [toRat_eq, Rat.mul_assoc]
     congr 1
     simp only [BitVec.toNat_shiftLeft_clz, Nat.shiftLeft_eq, Rat.natCast_mul, Rat.natCast_pow, Rat.natCast_ofNat, Rat.mul_assoc]
     congr
     rewrite [← Rat.zpow_natCast, ← Rat.zpow_add, ← Int.add_sub_assoc]
     congr 2
     have hle : -↑(2 ^ e : Nat) ≤ ↑uf.sig.clz.toNat * (2 : Int) := by grind
-    have hlt : ↑uf.sig.clz.toNat * (2 : Int) < ↑(2 ^ e : Nat) := by
-      have : uf.sig.clz.toNat ≤ s := by
-        suffices h : uf.sig.clz.toNat ≤ (↑s : BitVec s).toNat from by
-          simpa using h
-        simp only [← BitVec.ule_iff_toNat_le, BitVec.ule_iff_le, BitVec.clz_le]
-      grind [BitVec.clz_le, ← BitVec.ule_iff_le, BitVec.ule_iff_toNat_le]
-    rewrite [BitVec.toInt_sub_of_not_ssubOverflow, BitVec.toInt_setWidth, Int.bmod_eq_of_le_mul_two hle hlt]
+    rewrite [BitVec.toInt_sub_of_not_ssubOverflow]
+    · have hSigClzNeZero : uf.sig.clz < s := by
+        exact BitVec.clz_lt_iff_ne_zero.mpr h
+      have hSigClzNeZero' : uf.sig.clz.toNat < s := by
+        rw [BitVec.lt_def] at hSigClzNeZero
+        simp at hSigClzNeZero
+        apply hSigClzNeZero
+      rw [BitVec.toInt_setWidth]
+      have : (uf.sig.clz.toNat : Int).bmod (2 ^ e) = uf.sig.clz.toNat := by
+        refine Int.bmod_eq_of_le_mul_two hle ?_
+        norm_cast
+        grind
+      simp [this]
+      grind
     · grobner
-    · simpa using hex
-    · decide
+    · simp
 
 end UnpackedFloat
