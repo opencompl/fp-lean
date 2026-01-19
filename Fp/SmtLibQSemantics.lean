c01190d901dd3fc081850d80c4fe204226b1a1f0
Siddharth Bhat <siddu.druid@gmail.com>
2026-01-19 17:40:56 +0000

    chore: add Q defs

---
+++ Fp/SmtLibQSemantics.lean
@@ -0,0 +1,8 @@
+import Fp.Basic
+import Fp.Rounding
+
+/--
+We define the semantics of floating-point operations following the SMT-LIB
+style of semantics here. In particular, close attention is paid to being
+as close to the SMT-LIB definitions as possible.
+-/
