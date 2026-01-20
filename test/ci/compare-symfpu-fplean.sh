87606f7dc0f92277d707b640b49774efff39b2c1
Siddharth Bhat <siddu.druid@gmail.com>
2026-01-20 10:55:44 +0000

    chore: move Main to Test

--- test/ci/compare-symfpu-fplean.sh
+++ test/ci/compare-symfpu-fplean.sh
@@ -8,6 +8,6 @@ cd "$TOPLEVEL"
 shellcheck test/ci/compare-symfpu-fplean.sh
 make -C test/against-symfpu all
 ./test/against-symfpu/test.out > "$TOPLEVEL/golden-symfpu.csv"
-lake exec fp-lean e3m4 > "$TOPLEVEL/golden-fplean-against-symfpu.csv"
+lake exec fp-lean-test-suite e3m4 > "$TOPLEVEL/golden-fplean-against-symfpu.csv"
 test/ci/compare-against-golden.py "$TOPLEVEL/golden-symfpu.csv" "$TOPLEVEL/golden-fplean-against-symfpu.csv"
 
