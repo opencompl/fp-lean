e5b1013939ce161cadfec4849b1f70a591fc377f
Abdalrhman Mohamed <abdoo8080@outlook.com>
2026-01-26 06:00:32 -0800

    feat: simplify fma and addition circuits

--- Fp/FMA.lean
+++ Fp/FMA.lean
@@ -2,7 +2,7 @@ import Fp.Addition
 import Fp.Convert
 import Fp.Multiplication
 
-def UnpackedFloat.fma (sign : Bool) (x y z : UnpackedFloat e s) : UnpackedFloat (e + 2) (2 * s + 2) :=
+def UnpackedFloat.fma (x y z : UnpackedFloat e s) : UnpackedFloat (e + 2) (2 * s + 2) :=
   -- TODO: the renormalization step in `UnpackedFloat.add` requires `e + 2` to be large enough
   -- to store `-(2 ^ (e + 1) + 2 * s + 1)`. Is the `e` we get here from `exponentWidth` in
   -- `EUnpackedFloat.fma` large enough for the following inequality to hold?
@@ -10,49 +10,35 @@ def UnpackedFloat.fma (sign : Bool) (x y z : UnpackedFloat e s) : UnpackedFloat 
   -- After Substituting for `e` and `s`, we get:
   -- `-(2 ^ (exponentWidth e s + 1) + 2 * (s + 1) + 1) ≥ -2 ^ (exponentWidth e s + 2)`
   -- It seems to hold for up to `1000` bits.
-  UnpackedFloat.add sign (UnpackedFloat.mul x y) (z.extendWidths (by omega) (by omega))
+  UnpackedFloat.add (UnpackedFloat.mul x y) (z.extendWidths (by omega) (by omega))
 
 def EUnpackedFloat.fma (m : RoundingMode) (x y z : EUnpackedFloat (exponentWidth e s) (s + 1))
   : EUnpackedFloat (exponentWidth e s) (s + 1) :=
   let xySign := x.num.sign ^^ y.num.sign
-  -- Some of the cases below are subtle. So, we cannot simply copy the cases from `mul` and `add`.
-  -- Nesting the conditions helps us navigate the cases and ensure that we covered all of them.
-  bif x.isNaN || y.isNaN || x.isInfinite && y.isZero || y.isInfinite && x.isZero then
+  -- Some of the cases below are subtle. So, we cannot simply copy the cases from `mul` and/or `add`.
+  bif x.isNaN || y.isNaN || z.isNaN || x.isInfinite && y.isZero || y.isInfinite && x.isZero ||
+      (x.isInfinite || y.isInfinite) && z.isInfinite && xySign != z.sign then
     mkNaN
-  else bif x.isInfinite || y.isInfinite then
-    bif z.isNaN || z.isInfinite && xySign != z.sign then
-      mkNaN
-    else
-      mkInfinity xySign
+  else bif x.isInfinite || y.isInfinite || z.isInfinite then
+    mkInfinity (bif z.isInfinite then z.sign else xySign)
+  else bif (x.isZero || y.isZero) && z.isZero then
+    -- Result of the operation is an exact `0`. So, the sign depends on whether or not the signs agree.
+    -- If they agree, return the input sign. Otherwise, return the sign according to the rounding mode.
+    mkZero (bif xySign == z.sign then z.sign else m == .RTN)
   else bif x.isZero || y.isZero then
-    bif z.isNaN then
-      mkNaN
-    else bif z.isInfinite then
-      mkInfinity z.sign
-    else bif z.isZero then
-      -- Follow addition rules for the sign of an exact `0`.
-      mkZero (bif m == .RTN then xySign || z.sign else xySign && z.sign)
-    else
-      -- No rounding is needed here!
-      z
+    z
+  else bif z.isZero then
+    UnpackedFloat.round (.mul x.num y.num) m
+  else bif (.mul x.num y.num) == (z.num.neg.extendWidths (by omega) (by omega)) then
+    -- None of `x`, `y`, `x * y`, and `z` is an exact `0`. However, `x * y + z` is
+    -- *still* exactly `0`! We must follow addition rules for sign of `0` in that case.
+    -- Since `UnpackedFloat.mul` always returns exact results and extending ex/sig widths
+    -- also returns exact results, we can directly check if `x * y == -z`.
+    -- Note: this case can absorb the other `mkZero` case above (with a simpler sign!).
+    -- However, we keep it seperate as it's more expensive to check.
+    mkZero (m == .RTN)
   else
-    bif z.isNaN then
-      mkNaN
-    else bif z.isInfinite then
-      mkInfinity z.sign
-    else bif z.isZero then
-      -- Since `x` and `y` are not `0`, their exact product is also not `0`. However,
-      -- the product *could* round to `0`. Use the rounding sign in this case!
-      UnpackedFloat.round (.mul x.num y.num) m
-    else
-      -- None of `x`, `y`, `x * y`, and `z` is an exact `0`. However, `x * y + z` could
-      -- *still* be exactly `0`! We must follow addition rules for sign of `0` in that case.
-      -- If, however, `x * y + z` rounds to `0`, then we must return the sign of `x * y + z`.
-      -- Since `UnpackedFloat.mul` always returns exact results and `UnpackedFloat.add`
-      -- returns exact results when the exponents are the same (what happens when `expDiff` is `1`?),
-      -- So, we can leave it to `UnpackedFloat.add` to give the right sign to the resulting `0`.
-      -- TODO: why not just check if `x * y == -z` (and similarly for addition)?
-      UnpackedFloat.round (.fma (m == .RTN) x.num y.num z.num) m
+    UnpackedFloat.round (.fma x.num y.num z.num) m
 
 def PackedFloat.fma (m : RoundingMode) (x y z : PackedFloat e s) : PackedFloat e s :=
   (EUnpackedFloat.fma m x.unpack y.unpack z.unpack).pack
