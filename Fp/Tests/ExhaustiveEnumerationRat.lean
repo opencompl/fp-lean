b63b93c907588ebee98699eda7550ce5ff4b5630
Siddharth Bhat <siddu.druid@gmail.com>
2026-02-02 11:40:31 +0000

    chore: add ability to test for quality of rational approximation

--- Fp/Tests/ExhaustiveEnumerationRat.lean
+++ Fp/Tests/ExhaustiveEnumerationRat.lean
@@ -4,6 +4,7 @@ Exhuaustively enumerate semantics by rational number comparisons.
 import Fp.Basic
 import Fp.Addition
 import Fp.Multiplication
+import Fp.Division
 import Fp.Tests.PackedFloatEnumeration
 
 namespace Fp
@@ -17,28 +18,44 @@ structure UnpackedRatTestResult (ein sin : Nat) : Type where
   {eout : Nat}
   {sout : Nat}
   producedUnpacked : UnpackedFloat eout sout
-  expectedResult : Rat
+  expectedRat : Rat
 
 def UnpackedRatTestResult.producedRat (res : UnpackedRatTestResult ein sin) : Rat :=
   res.producedUnpacked.toRat
 
-/-- Check if the extended rationals 'e1, 'e2' are equal within the given precision. -/
-def ratWithinPrecision (r1 r2 : Rat) (prec : Nat) : Bool :=
-    let diff := (r1 - r2).abs
-    diff <= (2 : Rat) ^ (-(prec : Int))
 
 /--
-Return the precision difference magnitude between the produced and expected result.
+Return the numerical precision of the rational number.
+If the rational is less than `2^-i`, then it has precision at least i.
+returns None if the rational is zero.
 -/
-def UnpackedRatTestResult.precisionDifferenceMagnitude {ein sin : Nat}
-    (res : UnpackedRatTestResult ein sin) : Int :=
-  let diff := (res.producedRat - res.expectedResult).abs
-  let dyadic := diff.toDyadic (sin + 10)
-  dyadic.precision.getD 0
+def getRatPrecision (r : Rat) (maxPrec : Nat) : Option Nat :=
+  if r = 0 then none
+  else Id.run do
+    let mut prec : Nat := 0
+    for _ in [:maxPrec] do
+      if r.abs ≥ (1 : Rat) / (2 : Rat) ^ (prec : Nat) then
+        break
+      else
+        prec := prec + 1
+    return prec
 
+/--
+Return the precision of the produced result compared to the expected result.
+-/
+def UnpackedRatTestResult.precision {ein sin : Nat}
+      (res : UnpackedRatTestResult ein sin) (maxPrec : Nat) : Option Nat :=
+    let diff := (res.producedRat - res.expectedRat).abs
+    getRatPrecision diff maxPrec
+
+/--
+Check if the test result is a success, i.e., if the precision is at least `expectedPrecision`.
+-/
 def UnpackedRatTestResult.isSuccess {ein sin : Nat}
     (res : UnpackedRatTestResult ein sin) (expectedPrecision : Nat := sin + 2) : Bool :=
-  ratWithinPrecision res.producedRat res.expectedResult expectedPrecision
+  match res.precision (maxPrec := expectedPrecision * 2) with
+  | none => true
+  | some p => p >= expectedPrecision
 
 /--
 A summary of the results of applying an operation,
@@ -55,6 +72,16 @@ Make an empty summary for the given operation.
 -/
 def UnpackedRatTestSummary.empty (op : String) : UnpackedRatTestSummary ein sin :=
   { op := op }
+
+instance : Std.ToFormat Dyadic where
+  format d :=
+    match d.precision with
+    | none => "0"
+    | some prec =>
+      if prec < 0 then
+        f!"{d.numerator}/2^{prec.natAbs}"
+      else
+        f!"{d.numerator}*2^{prec.natAbs}"
 
 def UnpackedRatTestSummary.toFormat (summary : UnpackedRatTestSummary ein sin)
     (nFailedRecordsToPrint? : Option Nat := some 2) : Std.Format := Id.run do
@@ -76,8 +103,11 @@ def UnpackedRatTestSummary.toFormat (summary : UnpackedRatTestSummary ein sin)
     if nPrinted >= nFailedRecordsToPrint then
       out := out ++ f!"    ... (truncated, {summary.records.size - nPrinted} more failed records)\n"
       break
+    let maxPrec : Nat := sin * 5
+    let delta := (record.producedRat - record.expectedRat).abs
+    let deltaDyadic := delta.toDyadic maxPrec
     out := out ++
-      f!"    args: {formatArgs record.args}, produced: {record.producedRat}, expected: {record.expectedResult}, precision: {record.precisionDifferenceMagnitude}, expected precision {sin + 2} \n"
+      f!"    args: {formatArgs record.args}, produced: {record.producedRat}, expected: {record.expectedRat}, δ: {delta}={(Float.ofInt delta.num) / (Float.ofNat delta.den)}: {record.precision (maxPrec := maxPrec)}, expected precision {sin + 2} \n"
     nPrinted := nPrinted + 1
   return out
 where
@@ -127,6 +157,20 @@ def testMul (ein sin : Nat) : IO (UnpackedRatTestSummary ein sin) := do
       let result := UnpackedRatTestResult.mk (Array.mk [(pf1, r1), (pf2, r2)]) produced expected
       results := results.push result
   return summarizeUnpackedRatTestResults "mul" results
+
+def testDiv (ein sin : Nat) : IO (UnpackedRatTestSummary ein sin) := do
+  let mut results : Array (UnpackedRatTestResult ein sin) := #[]
+  let enum : PackedFloatEnumeration ein sin := PackedFloatEnumeration.mk ein sin
+  for (pf1, r1) in enum.enumeration do
+    for (pf2, r2) in enum.enumeration do
+      if r2 == 0 then continue
+      let uf1 := pf1.unpack.num
+      let uf2 := pf2.unpack.num
+      let produced := UnpackedFloat.div uf1 uf2
+      let expected := r1 / r2
+      let result := UnpackedRatTestResult.mk (Array.mk [(pf1, r1), (pf2, r2)]) produced expected
+      results := results.push result
+  return summarizeUnpackedRatTestResults "div" results
 
 end ExhaustiveEnumerationRat
 end Fp
