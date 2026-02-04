7eaafd3d506c9024e2f3ccc0137086539b016dea
Siddharth Bhat <siddu.druid@gmail.com>
2026-02-04 17:05:42 +0000

    chore: say what happens on NaN, infty

--- Fp/Theorems/UnpackedRound.lean
+++ Fp/Theorems/UnpackedRound.lean
@@ -4,8 +4,14 @@ import Fp.Theorems.SmtLibSemanticsQ
 
 namespace Fp
 
-theorem round_correct (inf : UnpackedFloat ein sin) (eout sout : Nat) (rm : RoundingMode) :
+theorem roundQ_eq_round_of_UnpackedFloat (inf : UnpackedFloat ein sin) (eout sout : Nat) (rm : RoundingMode) :
     Fp.SmtLibSemanticsQ.roundQ ein sin eout sout rm (EUnpackedFloat.mkNumber inf) =
       (UnpackedFloat.round inf rm).pack := by sorry
 
+theorem roundQ_eq_round_of_NaN (eout sout : Nat) (rm : RoundingMode) :
+    Fp.SmtLibSemanticsQ.roundQ 0 0 eout sout rm (EUnpackedFloat.mkNaN) = PackedFloat.mkNaN := by sorry
+
+  theorem roundQ_eq_round_of_Infinity (sign : Bool) (eout sout : Nat) (rm : RoundingMode) :
+    Fp.SmtLibSemanticsQ.roundQ 0 0 eout sout rm (EUnpackedFloat.mkInfinity sign) =
+      PackedFloat.getInfinity eout sout sign := by sorry
 end Fp
