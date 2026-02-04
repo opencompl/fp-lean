6569bfa46f4324309589f6f8972064fd93c4fdd5
Siddharth Bhat <siddu.druid@gmail.com>
2026-02-04 14:15:42 +0000

    chore: move stuff around to prepare for clean proofs of new stuff

--- Fp/Theorems/SmtLibSemanticsQ.lean
+++ Fp/Theorems/SmtLibSemanticsQ.lean
@@ -2,3 +2,8 @@
 Instantiation of the SMT-LIB semantics for rationals and packed floats.
 This will be where our proofs of correctness will be written against.
 -/
+import Fp.SmtLibSemantics
+
+
+namespace Rounding
+end Rounding
