7a45239e42cd248d3db61679ecf67de265293413
Siddharth Bhat <siddu.druid@gmail.com>
2026-02-04 18:00:22 +0000

    chore: add another sorry'd defn to try proofs on

--- Fp/Theorems/UnpackedRound.lean
+++ Fp/Theorems/UnpackedRound.lean
@@ -1,6 +1,7 @@
 import Fp.UnpackedRound
 import Fp.SmtLibSemantics
 import Fp.Theorems.SmtLibSemanticsQ
+import Fp.Multiplication
 
 namespace Fp
 
@@ -11,7 +12,15 @@ theorem roundQ_eq_round_of_UnpackedFloat (inf : UnpackedFloat ein sin) (eout sou
 theorem roundQ_eq_round_of_NaN (eout sout : Nat) (rm : RoundingMode) :
     Fp.SmtLibSemanticsQ.roundQ 0 0 eout sout rm (EUnpackedFloat.mkNaN) = PackedFloat.mkNaN := by sorry
 
-  theorem roundQ_eq_round_of_Infinity (sign : Bool) (eout sout : Nat) (rm : RoundingMode) :
+theorem roundQ_eq_round_of_Infinity (sign : Bool) (eout sout : Nat) (rm : RoundingMode) :
     Fp.SmtLibSemanticsQ.roundQ 0 0 eout sout rm (EUnpackedFloat.mkInfinity sign) =
       PackedFloat.getInfinity eout sout sign := by sorry
+
+/--
+Example theorem we will prove, using our proof strategy of proving against the SMT-Lib semantics.
+-/
+theorem mul_eq_mul (eout sout : Nat) (rm : RoundingMode) (a b : PackedFloat ein sin) :
+  Fp.SmtLibSemantics.SmtLibFunctions.mul (Fp.SmtLibSemanticsQ.smtLibRoundMethodQ ein sin)
+    rm a b = PackedFloat.mul rm a b
+     := by sorry
 end Fp
