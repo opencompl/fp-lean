3405d21d96d6a7e72b2c327c8b3701db8e64d0ae
Siddharth Bhat <siddu.druid@gmail.com>
2026-04-16 15:21:07 +0100

    chore: establish what happens at boundary

--- Fp/Theorems/Successor.lean
+++ Fp/Theorems/Successor.lean
@@ -8,12 +8,18 @@ import Fp.Theorems.Ordering
 namespace Fp
 namespace PackedFloat
 
-@[simp]
+/--
+successorAwayFromZero of NaN is NaN
+-/
+@[simp, grind .]
 theorem successorAwayFromZero_eq_of_isNaN (pf : PackedFloat e s) (hNaN : pf.isNaN) :
     pf.successorAwayFromZero = pf := by
   simp  [PackedFloat.successorAwayFromZero, hNaN]
 
-@[simp]
+/--
+successorAwayFromZero of ∞ is ∞
+-/
+@[simp, grind .]
 theorem successorAwayFromZero_eq_of_isInfinite (pf : PackedFloat e s)
     (hInf : pf.isInfinite) :
     pf.successorAwayFromZero = pf := by
@@ -22,12 +28,25 @@ theorem successorAwayFromZero_eq_of_isInfinite (pf : PackedFloat e s)
     → PackedFloat.unpack_eq_NaN_of_isNaN, !PackedFloat.unpack_getInfinity,
     !PackedFloat.isInfinite_getInfinity, !PackedFloat.isInfinite_unpack_eq_isInfinite, #532a]
 
-theorem successorAwwayFromZero_maxNormal_eq (he : 0 < e) (hs : 0 < s) (sign : Bool) :
+/--
+successorAwayFromZero of max normal number is +∞.
+-/
+@[simp, grind .]
+theorem successorAwayFromZero_maxNormal_eq (he : 1 < e) (sign : Bool) :
     (PackedFloat.maxNormalNumber e s sign).successorAwayFromZero =
     PackedFloat.getInfinity e s sign := by
   simp only [PackedFloat.successorAwayFromZero]
   have : ¬ (PackedFloat.maxNormalNumber e s sign).isNaN := by
     grind
+  simp [this]
+  intros hmax
+  intros hsub
+  apply Classical.byContradiction
+  intros hcontra
+  apply hcontra
+  simp [PackedFloat.getInfinity]
+  grind only
+
 
 
 theorem toRat_successorAwayFromZero_eq
